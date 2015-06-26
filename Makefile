0323663dad4e5fcd2c18d6993808629ec99a66ec
grabner <pjg.github@ubergrabner.net>
2015-06-26 12:07:39 -0400

    added packaging files

---
+++ Makefile
@@ -0,0 +1,16 @@
+PROJECT = lessc
+
+test:
+	nosetests --verbose
+
+upload:
+	python setup.py sdist upload
+
+clean:
+	find . -iname '*.pyc' -exec rm {} \;
+	rm -fr "$(PROJECT).egg-info" dist
+
+tag:
+	@echo "[  ] tagging as version `cat VERSION.txt`..."
+	git tag -a "v`cat VERSION.txt`" -m "released v`cat VERSION.txt`"
+
---
+++ setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# file: $Id$
+# auth: Philip J Grabner <phil@canary.md>
+# date: 2015/06/26
+# copy: (C) Copyright 2015-EOT Canary Health, Inc., All Rights Reserved.
+#------------------------------------------------------------------------------
+
+import os, sys, setuptools
+from setuptools import setup, find_packages
+
+# require python 2.7+
+if sys.hexversion < 0x02070000:
+  raise RuntimeError('This package requires python 2.7 or better')
+
+heredir = os.path.abspath(os.path.dirname(__file__))
+def read(*parts, **kw):
+  try:    return open(os.path.join(heredir, *parts)).read()
+  except: return kw.get('default', '')
+
+test_dependencies = [
+  'nose                 >= 1.3.0',
+  'coverage             >= 3.5.3',
+]
+
+dependencies = [
+  'distribute           >= 0.6.24',
+  'asset                >= 0.6',
+]
+
+classifiers = [
+  'Intended Audience :: Developers',
+  'Programming Language :: Python',
+  'Operating System :: OS Independent',
+  'Topic :: Internet',
+  'Topic :: Software Development',
+  'Topic :: Internet :: WWW/HTTP',
+  'Natural Language :: English',
+  'License :: OSI Approved :: MIT License',
+  'License :: Public Domain',
+]
+
+setup(
+  name                  = 'lessc',
+  version               = read('VERSION.txt', default='0.0.1').strip(),
+  description           = 'A Python less compiler',
+  long_description      = read('README.rst'),
+  classifiers           = classifiers,
+  author                = 'Philip J Grabner, Canary Health Inc',
+  author_email          = 'oss@canary.md',
+  url                   = 'http://github.com/canaryhealth/lessc',
+  keywords              = 'less css compiler',
+  packages              = find_packages(),
+  include_package_data  = True,
+  zip_safe              = True,
+  install_requires      = dependencies,
+  tests_require         = test_dependencies,
+  test_suite            = 'lessc',
+  entry_points          = '',
+  license               = 'MIT (http://opensource.org/licenses/MIT)',
+)
+
+#------------------------------------------------------------------------------
+# end of $Id$
+# $ChangeLog$
+#------------------------------------------------------------------------------
