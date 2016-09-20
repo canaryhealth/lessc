b97281759292dcb7e43f5224e385ef6298767922
grabner <pjg.github@ubergrabner.net>
2016-09-20 12:17:02 -0400

    added dev status classifiers

--- setup.py
+++ setup.py
@@ -30,6 +30,10 @@ dependencies = [
 ]
 
 classifiers = [
+  'Development Status :: 3 - Alpha',
+  # 'Development Status :: 4 - Beta',
+  # 'Development Status :: 5 - Production/Stable',
+  'Environment :: Console',
   'Intended Audience :: Developers',
   'Programming Language :: Python',
   'Operating System :: OS Independent',
